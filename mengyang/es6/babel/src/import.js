ebc8e00cf81a96feb592e1d0b08e72b212e9322b
lizushanCSDN <2749642414@qq.com>
2020-04-05 22:25:47 +0800

    练习

---
+++ mengyang/es6/babel/src/import.js
@@ -0,0 +1,10 @@
+// import {a,foo} from "./export";
+// console.log(a); 
+// console.log(foo());
+
+// import {a as c} from "./export";
+// console.log(c);
+
+import Teacher from "./export";
+let t1 = new Teacher("小张");
+t1.showName();
---
+++ mengyang/es6/babel/src/index.js
@@ -0,0 +1,12 @@
+let a = 10;
+class Person{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    shoeName(){
+        console.logO(this.name);
+    }
+}
+
+let [q,e,r] = [1,2,3];
