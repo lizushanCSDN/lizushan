c47c8985ea60aa9f34969480051cdaa18953c017
lizushanCSDN <2749642414@qq.com>
2020-04-07 18:57:13 +0800

    admin

---
+++ mengyang/webpack_text/src/index.js
@@ -0,0 +1,5 @@
+import css from "./css/main.css"
+import scss from "./style/main.scss"
+let na = 10;
+document.querySelector("#box").innerHTML="<h1>hello world fellow41</h1>";
+$("#test-text").html("<h1>你好</h1>")
---
+++ mengyang/webpack_text/webpack.config.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const HtmlPlugins = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require("webpack");
+const glob = require("glob");
+const PurifyCSSPlugin = require("purifycss-webpack");
+const entry = require("./webpack_config/entry");
+module.exports = {
+    mode:"development",
+    entry,
+    output:{
+        path:path.resolve(__dirname,"dist"),
+        filename:'[name].js',
+        publicPath:"http://127.0.0.1:8081/"
+    },
+    plugins:[
+        new HtmlPlugins({
+            minify:{
+                removeAttributeQuotes:true
+            },
+            hash:true,
+            template:"./src/index.html"
+        }),
+        new ExtractTextPlugin("css/main.css"),
+        new webpack.ProvidePlugin({
+          $:"jquery"
+        }),
+        new PurifyCSSPlugin({
+          paths:glob.sync(path.join(__dirname,"./src/*.html"))
+        })
+    ],
+    module:{
+        rules:[
+          {
+            test:/\.css$/,
+            // use:['style-loader','css-loader']
+            use:ExtractTextPlugin.extract({
+              fallback:"style-loader",
+              use:[{
+                loader:"css-loader",
+                options:{importLoaders:1}
+              },"postcss-loader"]
+            })
+          },
+          {
+            test:/\.(png|jpg|gif)$/,
+            use:[
+              {
+                loader:"url-loader",
+                options:{
+                  limit:500,
+                  outputPath:"images/",
+                  esModule:false
+                }
+              }
+            ]
+          },
+          {
+            test:/\.(htm|html)$/,
+            loader:"html-withimg-loader"
+          },
+          {
+            test:/\.scss$/,
+            // use:["style-loader","css-loader","sass-loader"]
+            use:ExtractTextPlugin.extract({
+              use:["css-loader","sass-loader"],
+              fallback:"style-loader"
+            })
+          },
+          {
+            test:/\.js$/,
+            use:[{
+              loader:"babel-loader",
+              options:{
+                presets:['@babel/preset-env']
+              }
+            }],
+            exclude:"/node_modules/"
+          }
+        ]
+      },
+    devServer:{
+        contentBase:path.resolve(__dirname,"dist"),
+        host:"127.0.0.1",
+        compress:true,
+        port:"8081",
+        open:true
+    }
+}
