c834b85fefe99b6e71cefe5dadbc4628c6620cc0
lizushanCSDN <2749642414@qq.com>
2020-04-10 16:04:00 +0800

    vuecli项目

---
+++ mengyang/vue-cli/vuecli3-text/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="active">你好</div>
+    <!-- 原来的代码 -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// 原来的代码
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "Home",
+  // 原来的代码
+  // components: {
+  //   HelloWorld
+  // }
+  components: {}
+};
+</script>
+<style>
+  .active{
+    color:blue;
+  }
+</style>
